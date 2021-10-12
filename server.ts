46ef32d0231ac61dc2fe67dec8352563317edbba
Techit Kakaew <Techit@admins-MacBook-Pro.local>
2021-10-12 14:25:56 +0700

    fix server

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const express = require("express");
+const port = process.env.PORT || 8080;
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 app.listen(port);
+
+console.log("Server started...");
